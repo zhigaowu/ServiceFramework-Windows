0f1e053f5324fa7e7337a6b7e01ed64475011970
zhigaowu <wuzhigaoem@163.com>
2020-10-09 17:05:46 +0800

    1, delete ServiceDefinition.json 2, add decompress logic(not complete)

--- ServiceManager.BS/src/Http/HttpService.cpp
+++ ServiceManager.BS/src/Http/HttpService.cpp
@@ -232,10 +232,79 @@ static void GetLastErrorString(rapidjson::Value& err, rapidjson::Document::Alloc
 #endif
 }
 
-struct file_writer_data {
-    FILE *fp;
-    size_t bytes_written;
-};
+static int archive_copy_data(struct archive *ar, struct archive *aw)
+{
+    int r;
+    const void *buff;
+    size_t size;
+    la_int64_t offset;
+
+    for (;;) {
+        r = archive_read_data_block(ar, &buff, &size, &offset);
+        if (r == ARCHIVE_EOF)
+            return (ARCHIVE_OK);
+        if (r < ARCHIVE_OK)
+            return (r);
+        r = archive_write_data_block(aw, buff, size, offset);
+        if (r < ARCHIVE_OK) {
+            fprintf(stderr, "%s\n", archive_error_string(aw));
+            return (r);
+        }
+    }
+}
+
+static void archive_extract(const char *filename)
+{
+    struct archive *a;
+    struct archive *ext;
+    struct archive_entry *entry;
+    int flags;
+    int r;
+
+    /* Select which attributes we want to restore. */
+    flags = ARCHIVE_EXTRACT_TIME;
+    flags |= ARCHIVE_EXTRACT_PERM;
+    flags |= ARCHIVE_EXTRACT_ACL;
+    flags |= ARCHIVE_EXTRACT_FFLAGS;
+
+    a = archive_read_new();
+    archive_read_support_format_all(a);
+    archive_read_support_compression_all(a);
+    ext = archive_write_disk_new();
+    archive_write_disk_set_options(ext, flags);
+    archive_write_disk_set_standard_lookup(ext);
+    if ((r = archive_read_open_filename(a, filename, 10240)))
+        exit(1);
+    for (;;) {
+        r = archive_read_next_header(a, &entry);
+        if (r == ARCHIVE_EOF)
+            break;
+        if (r < ARCHIVE_OK)
+            fprintf(stderr, "%s\n", archive_error_string(a));
+        if (r < ARCHIVE_WARN)
+            exit(1);
+        r = archive_write_header(ext, entry);
+        if (r < ARCHIVE_OK)
+            fprintf(stderr, "%s\n", archive_error_string(ext));
+        else if (archive_entry_size(entry) > 0) {
+            r = archive_copy_data(a, ext);
+            if (r < ARCHIVE_OK)
+                fprintf(stderr, "%s\n", archive_error_string(ext));
+            if (r < ARCHIVE_WARN)
+                exit(1);
+        }
+        r = archive_write_finish_entry(ext);
+        if (r < ARCHIVE_OK)
+            fprintf(stderr, "%s\n", archive_error_string(ext));
+        if (r < ARCHIVE_WARN)
+            exit(1);
+    }
+    archive_read_close(a);
+    archive_read_free(a);
+    archive_write_close(ext);
+    archive_write_free(ext);
+    //exit(0);
+}
 
 static void ev_upload(struct mg_connection *nc, int ev, void *p) 
 {
@@ -2020,7 +2089,26 @@ std::list<std::string> HttpService::parse_dependent_service_definitions(struct a
     struct archive_entry *entry = nullptr;
     while (archive_read_next_header(ar, &entry) == ARCHIVE_OK) 
     {
-        related_definitions.emplace_back(archive_entry_pathname(entry));
+        const char* pathname = archive_entry_pathname(entry);
+        size_t pathname_len = strlen(pathname);
+        // is library
+        if (pathname_len > 4 && pathname[pathname_len - 4] == '.' && pathname[pathname_len - 3] == 'd' && pathname[pathname_len - 2] == 'l' && pathname[pathname_len - 1] == 'l')
+        {
+            // top dir/services/service template id/service.dll
+            const char* first_slash = std::find(pathname, pathname + pathname_len, '/');
+            if (first_slash != pathname + pathname_len)
+            {
+                const char* second_slash = std::find(first_slash + 1, pathname + pathname_len, '/');
+                if (second_slash != pathname + pathname_len && strncmp(first_slash + 1, "services", 8) == 0)
+                {
+                    const char* third_slash = std::find(second_slash + 1, pathname + pathname_len, '/');
+                    if (third_slash != pathname + pathname_len)
+                    {
+                        related_definitions.emplace_back(std::string(second_slash + 1, third_slash));
+                    }
+                }
+            }
+        }
         archive_read_data_skip(ar);  // Note 2
     }
     return related_definitions;
@@ -2090,6 +2178,9 @@ bool HttpService::stop_dependent_services(const std::list<std::string>& related_
                 {
                     code.SetInt(Service_Module_Failed);
                     GetLastErrorString(err, allo);
+
+                    CloseServiceHandle(schService);
+                    break;
                 }
             }
             
